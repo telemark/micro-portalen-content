69833762531efbeded866a9f798c971f3b0d2f86
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-03-01 13:24:24 +0100

    Updates content url (patch)

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  contentUrl: process.env.CONTENT_URL || 'http://portaleninfo.com',
+  contentUrl: process.env.CONTENT_URL || 'https://portaleninfo.com',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'portalen',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
