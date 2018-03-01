f05f58e3fb8ec387567752c05bfc68bf9aea21f7
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-03-01 12:31:00 +0100

    Switches from craft to wordpress (major)

--- utils/build-tags-mapping.js
+++ utils/build-tags-mapping.js
@@ -3,12 +3,13 @@ const { writeFileSync } = require('fs')
 const config = require('../config')
 const tagsUrl = `${config.contentUrl}/wp-json/wp/v2/tags`
 
-function repackTags (item) {
-  return Object.assign({}, {[item.name]: item.id})
+function repackTags (prev, curr) {
+  prev[curr.name] = curr.id
+  return prev
 }
 
 axios.get(tagsUrl).then(result => {
-  const tags = result.data.map(repackTags)
+  const tags = result.data.reduce(repackTags, {})
   writeFileSync('lib/data/tags-mapping.json', JSON.stringify(tags, null, 2))
   console.log('tags-mapping complete')
 }).catch(console.error)
