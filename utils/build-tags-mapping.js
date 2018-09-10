9714c989282cf6f9541bc7e7bd07a049ed487963
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-09-10 07:09:32 +0200

    Fixes utils (patch)

--- utils/build-tags-mapping.js
+++ utils/build-tags-mapping.js
@@ -10,6 +10,6 @@ function repackTags (prev, curr) {
 
 axios.get(tagsUrl).then(result => {
   const tags = result.data.reduce(repackTags, {})
-  writeFileSync('lib/data/tags-mapping.json', JSON.stringify(tags, null, 2))
+  writeFileSync('src/lib/data/tags-mapping.json', JSON.stringify(tags, null, 2))
   console.log('tags-mapping complete')
 }).catch(console.error)
